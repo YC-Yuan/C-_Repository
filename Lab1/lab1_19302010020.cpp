62c56aab79a030cb25dc4b6400be9e5f172b9d01
yicongyuan <19302010020@fudan.edu.cn>
2021-04-13 19:03:58 +0800

    lab1重新入档 2048Lab改名

---
+++ Lab1/lab1_19302010020.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <random>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Matrix {
+public:
+    void print() {
+        print_baseline();
+        print_line(0);
+        print_baseline();
+        print_line(1);
+        print_baseline();
+        print_line(2);
+        print_baseline();
+        print_line(3);
+        print_baseline();
+        if (num_false == 0) {
+            cout << "game over" << endl;
+            exit(0);
+        }
+    }
+
+    void change_one() {
+        vector<int> numbers{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        shuffle(numbers.begin(), numbers.end(), std::mt19937(std::random_device()()));
+        for (int i = 0; i < 16; ++i) {
+            if (!array[numbers.at(i)]) {
+                array[numbers.at(i)] = true;
+                break;
+            }
+        }
+        num_false--;
+    }
+
+private:
+    int num_false = 16;
+    bool array[16] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+                      false, false,};
+
+    void print_one(int num) {
+        if (array[num]) cout << '2';
+        else cout << ' ';
+    };
+
+    static void print_baseline() {
+        cout << "+-----+-----+-----+-----+" << endl;
+    };
+
+    void print_line(int num) {
+        num = num * 4;
+        cout << "|  ";
+        print_one(num);
+        cout << "  |  ";
+        print_one(num + 1);
+        cout << "  |  ";
+        print_one(num + 2);
+        cout << "  |  ";
+        print_one(num + 3);
+        cout << "  |" << endl;
+    }
+};
+
+int main() {
+    Matrix mt;
+    mt.print();
+
+    for (int i = 0; i < 100; ++i) {
+        {
+            string input;
+            cin >> input;
+            if (input == "n") {
+                mt.change_one();
+                mt.print();
+            } else {
+                cout << "非法输入，请输入n" << endl;
+            }
+        }
+    }
+    cout << "真有耐心！不跟你玩儿了" << endl;
+}
